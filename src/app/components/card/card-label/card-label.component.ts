f378fa91706f6ddd55c80c307e1fc69878ef0779
JamissonBarbosa <jamissondasilvafilho@gmail.com>
2024-09-13 07:56:44 -0300

    feat: create card price

--- src/app/components/card/card-label/card-label.component.ts
+++ src/app/components/card/card-label/card-label.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-label',
   standalone: true,
   imports: [],
   templateUrl: './card-label.component.html',
-  styleUrl: './card-label.component.scss'
+  styleUrl: './card-label.component.scss',
 })
-export class CardLabelComponent {
+export class CardLabelComponent implements OnInit {
+  @Input()
+  gameLabel: string = '';
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { AngularMaterialModule } from '../../shared/angular-material/angular-material.module';
+import { CardLabelComponent } from './card-label/card-label.component';
+import { CardPriceComponent } from './card-price/card-price.component';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [AngularMaterialModule],
+  imports: [AngularMaterialModule, CardLabelComponent, CardPriceComponent],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
